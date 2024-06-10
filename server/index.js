6f1d727809db627061ee9b493a94f8b7c83e4371
harshita187 <harsita129singh@gmail.com>
2024-06-10 20:09:47 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv')
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }).then(() => console.log('MongoDB connected'))
+//     .catch((err) => console.log(err));
+
+
+app.get("/getData",(req,res)=>{
+    res.send("Hello I'm harshita");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
+
+  
